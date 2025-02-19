e70b5efe5e2acb8ecafcf1f51be248e679e8eeed
Nimesha Kumarapperuma <nimeshamadhushani100@gmail.com>
2025-02-19 09:33:29 +0530

    Update the config file with including view ports of the browsers

--- playwright.config.js
+++ playwright.config.js
@@ -40,17 +40,28 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { 
+        ...devices['Desktop Chrome'],
+        viewport:{width:1536,height:864}
+      },
+     
     },
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: { 
+        ...devices['Desktop Firefox'] ,
+        viewport:{width:1536,height:864}
+      },
+      
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      use: { 
+        ...devices['Desktop Safari'],
+        viewport:{width:1536,height:864} 
+      },
     },
 
     /* Test against mobile viewports. */
